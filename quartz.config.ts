4c02f43b8b227462bc26a09bc2f9ef1104086c43
corvusdeinanis <25402014+corvusdeinanis@users.noreply.github.com>
2023-08-22 10:54:52 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "astrodex",
+    pageTitle: "📓astrodex",
     enableSPA: true,
     enablePopovers: true,
     baseUrl: "cuddlyspaceship.netlify.app",
