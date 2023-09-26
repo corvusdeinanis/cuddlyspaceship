0a85102fdcfd01dce53d65e5457ccb1bd7efbf24
corvusdeinanis <25402014+corvusdeinanis@users.noreply.github.com>
2023-09-27 02:46:43 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,9 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/corvusdeinanis/cuddlyspaceship",
+      Website: "https://astroslair.xyz",
+      //"Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
