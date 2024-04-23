7f68dcbb53cc6128d28340a7fd87783d1b158c39
corvusdeinanis <25402014+corvusdeinanis@users.noreply.github.com>
2024-04-23 21:35:05 +0530

    trying to get last updated from git

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,6 +14,7 @@ const config: QuartzConfig = {
     baseUrl: "corvusdeinanis.github.io/cuddlyspaceship",
     ignorePatterns: ["private", "templates",".obsidian"],
 	defaultDateType: "created",
+	displayDateType:["created","modified"]
 	analytics: {
 	},
     theme: {
@@ -52,7 +53,8 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+	  
+        priority: ["frontmatter", "git"],
       }),
       //Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
